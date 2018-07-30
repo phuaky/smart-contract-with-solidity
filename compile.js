4d4c23aa96e39e421ae0b7cf696d76925ae09abc
phuaky <championpky@gmail.com>
2018-07-30 18:35:51 +0800

    lesson 49

--- compile.js
+++ compile.js
@@ -3,6 +3,6 @@ const fs = require('fs'); //use file system from node
 const solc = require('solc'); //use solc library installed on node earlier
 
 const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol'); //draw the path to the file inbox.sol
-const source = fs.readFileSync(inboxPath, 'utf8'); //use the right encoding to read the file 
+const source = fs.readFileSync(inboxPath, 'utf8'); //use the right encoding to read the file
 
 module.exports = solc.compile(source, 1).contracts[':Inbox'];
--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,25 +1,46 @@
 const assert = require('assert'); //node module - make assertion of tests - to make sure 1 value = another
 const ganache = require('ganache-cli'); //local ethereum test network
-const Web3 = require ('web3'); // constructor - create instances of Web3 library
+const Web3 = require('web3'); // constructor - create instances of Web3 library
 const web3 = new Web3(ganache.provider());
-const { interface, bytecode } = require('../compile');
+const {
+  interface,
+  bytecode
+} = require('../compile');
 
 let accounts;
 let inbox;
 
-beforeEach(async () => {
+beforeEach(async() => {
   // Get a list of all accounts
-    accounts = await web3.eth.getAccounts()
+  accounts = await web3.eth.getAccounts()
 
   // Use one of those accounts to deploy the contract
   inbox = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode, arguments: ['Hi there!'] })
-    .send({ from: accounts[0], gas: '1000000' })
-  })
+    .deploy({
+      data: bytecode,
+      arguments: ['Hi there!']
+    })
+    .send({
+      from: accounts[0],
+      gas: '1000000'
+    })
+})
 
 describe('Inbox', () => {
   it('deploys a contract', () => {
     assert.ok(inbox.options.address);
+  })
 
-    }) 
+  it('has a default message', async() => {
+    const message = await inbox.methods.message().call();
+    assert.equal(message, 'Hi there!');
+  })
+
+  it('can change the message', async() => {
+    await inbox.methods.setMessage('bye').send({
+      from: accounts[0]
+    })
+    const message = await inbox.methods.message().call();
+    assert.equal(message, 'bye')
+  })
 })
